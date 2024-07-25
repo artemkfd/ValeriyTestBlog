098b3fe92530939e41a901ede2659d68ab51b7ae
artemkfd <artemkfd@gmail.com>
2024-07-25 16:24:37 +0300

    add postgresql db

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
+
 
 
 # Password validation
