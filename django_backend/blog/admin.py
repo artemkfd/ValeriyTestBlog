c6a693aa0861aa3c869a3090a1fa8666f58e27e9
artemkfd <artemkfd@gmail.com>
2024-07-25 17:24:43 +0300

    add models and admin panel

--- django_backend/blog/admin.py
+++ django_backend/blog/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.db.models import CharField, TextField
+from blog.models import Post, Comment, Category
 
-# Register your models here.
+admin.site.register(Category)
+
+class PostFieldsListFilter(admin.SimpleListFilter):
+    title = 'Post Fields'
+    parameter_name = 'post_fields'
+
+    def lookups(self, request, model_admin):
+        # Возвращает два значения: идентификатор и отображаемый текст
+        return [('all', 'Все поля')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'all':
+            # Используйте метод only() для включения всех полей модели
+            return queryset.only(*[field.name for field in Post._meta.fields if isinstance(field, (CharField, TextField))])
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'text',
+                    'category',
+                    'author',
+                    'created_at',
+                    'updated_at',
+                    )
+    search_fields = ['title',
+                     'text',
+                     'created_at',
+                     'updated_at']
+    list_filter = [PostFieldsListFilter]
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentFieldsListFilter(admin.SimpleListFilter):
+    title = 'Comment Fields'
+    parameter_name = 'comment_fields'
+
+    def lookups(self, request, model_admin):
+        # Возвращает два значения: идентификатор и отображаемый текст
+        return [('all', 'Все поля')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'all':
+            # Используйте метод only() для включения всех полей модели
+            return queryset.only(*[field.name for field in Post._meta.fields if isinstance(field, (CharField, TextField))])
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text',
+                    'post',
+                    'author',
+                    'created_at',
+                    'updated_at',
+                    )
+    search_fields = ['text',
+                     'created_at',
+                     'updated_at']
+    list_filter = [CommentFieldsListFilter]
+
+admin.site.register(Comment, CommentAdmin)
--- django_backend/blog/models.py
+++ django_backend/blog/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    text = models.CharField('Текст',max_length=255)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField('дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('дата обновления', auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    title = models.CharField('Заголовок', max_length=255)
+    text = models.TextField('Текст')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = AutoSlugField(populate_from=['title'])
+    created_at = models.DateTimeField('дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('дата обновления', auto_now=True)
+
+    def __str__(self):
+        return self.title
